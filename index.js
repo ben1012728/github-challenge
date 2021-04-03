8a38ad167a44791784905bce6d29c9a98b4ec247
ben1012728 <81409042+ben1012728@users.noreply.github.com>
2021-04-04 01:30:17 +0900

    2020.04.03
    js를 이용한 데이터 정렬방식과 기본문법 사용

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-Console.log('Im Working. Im Beautiful. Im worth it');
+const benInfo ={
+    name:"ben",
+    age:21,
+    gender:"male",
+    isHandosme:true,
+    favMovies: ["withGod","kingOfFighter"]
+};
+console.log(benInfo.favMovies[0], 
